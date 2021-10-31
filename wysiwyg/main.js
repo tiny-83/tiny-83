b6672898003d100b14407dbe923ef542121a066c
fun <lljxx1@gmail.com>
2021-10-31 11:41:54 +0800

    editor support drag

--- wysiwyg/main.js
+++ wysiwyg/main.js
@@ -4,14 +4,14 @@ let grid = [];
 let leftCode = null;
 let rightCode = null;
 
-window.paintConf = null
+window.paintConf = null;
 
 $(document).ready(() => {
   for (let i = 0; i < H; i++) {
-    $('#screen').append(`<tr data-i="${i}"></tr>`);
+    $("#screen").append(`<tr data-i="${i}"></tr>`);
     for (let j = 0; j < W; j++) {
       const dom = $(`<td data-j="${j}" class="off"></td>`);
-      dom.on('click', handleClick)
+      dom.on("click", handleClick);
       $("#screen tr").last().append(dom);
     }
   }
@@ -19,7 +19,9 @@ $(document).ready(() => {
   function drawGrid() {
     for (let i = 0; i < H; i++) {
       for (let j = 0; j < W; j++) {
-        $(`tr[data-i=${i}] td[data-j=${j}]`).removeClass().addClass(grid[i][j] ? 'on' : 'off');
+        $(`tr[data-i=${i}] td[data-j=${j}]`)
+          .removeClass()
+          .addClass(grid[i][j] ? "on" : "off");
       }
     }
   }
@@ -29,17 +31,41 @@ $(document).ready(() => {
     const row = parseInt($(this).parent().attr("data-i"));
     const cell = parseInt($(this).attr("data-j"));
     grid[row][cell] = !grid[row][cell];
-    let nowStatus = grid[row][cell]; 
+    let nowStatus = grid[row][cell];
     cellDom.removeClass().addClass(nowStatus ? "on" : "off");
     if (window.paintConf) {
-      if (nowStatus){
+      if (nowStatus) {
         cellDom.css("background-color", "#" + window.paintConf.pixelColor);
-      }else{
+      } else {
         cellDom.css("background-color", window.paintConf.screenColor);
       }
-    } 
+    }
     updateCode();
   }
+
+  function bindDrag() {
+    const screen = document.querySelector("#screen");
+    let isMouseDown = false;
+    screen.addEventListener("mouseup", function () {
+      isMouseDown = false;
+    });
+    screen.addEventListener("mouseleave", function () {
+      isMouseDown = false;
+    });
+
+    screen.addEventListener("mousedown", function (e) {
+      isMouseDown = true;
+      screen.addEventListener("mouseover", function (e) {
+        if (isMouseDown) {
+          if (e.target.tagName === "TD") {
+            handleClick.apply(e.target);
+          }
+        }
+      });
+    });
+  }
+
+  bindDrag();
 
   function resetGrid() {
     grid = [];
@@ -54,19 +80,20 @@ $(document).ready(() => {
 
   function updateGrid() {
     resetGrid();
-    const xMin = Number($('#x-min').val());
-    const xMax = Number($('#x-max').val());
-    const yMin = Number($('#y-min').val());
-    const yMax = Number($('#y-max').val());
+    const xMin = Number($("#x-min").val());
+    const xMax = Number($("#x-max").val());
+    const yMin = Number($("#y-min").val());
+    const yMax = Number($("#y-max").val());
     const nbPointsPerPixel = 16;
-    const pixelWidth = (xMax-xMin) / W;
+    const pixelWidth = (xMax - xMin) / W;
     for (let j = 0; j < W; j++) {
       for (let jIdx = 1; jIdx <= nbPointsPerPixel; jIdx++) {
-        const x = xMin + j * pixelWidth + (jIdx / (nbPointsPerPixel + 1)) * pixelWidth;
-        const f = new Function('x', 'return ' + $('#user-function').val());
+        const x =
+          xMin + j * pixelWidth + (jIdx / (nbPointsPerPixel + 1)) * pixelWidth;
+        const f = new Function("x", "return " + $("#user-function").val());
         const y = f(x);
         if (y !== undefined && y < yMax && y > yMin) {
-          const i = H - 1 - Math.floor(H * (y - yMin) / (yMax - yMin));
+          const i = H - 1 - Math.floor((H * (y - yMin)) / (yMax - yMin));
           grid[i][j] = true;
         }
       }
@@ -74,14 +101,18 @@ $(document).ready(() => {
   }
 
   function gridToPairOfUint160() {
-    let gridR = grid.map((val, index) => [grid].map(row => row[index]).reverse());
-    grid2R = gridR[0].map((val, index) => gridR.map(row => row[index]).reverse())[0];
+    let gridR = grid.map((val, index) =>
+      [grid].map((row) => row[index]).reverse()
+    );
+    grid2R = gridR[0].map((val, index) =>
+      gridR.map((row) => row[index]).reverse()
+    )[0];
     let leftPart = 0n;
     let rightPart = 0n;
     for (let i = 0; i < 14; i++) {
       for (let j = 0; j < 22; j++) {
         const power = BigInt(13 - i) + 14n * BigInt(j % 11);
-        const diff = grid2R[i][j] ? (2n ** power) : 0n;
+        const diff = grid2R[i][j] ? 2n ** power : 0n;
         if (j < 11) {
           leftPart += diff;
         } else {
@@ -120,26 +151,26 @@ $(document).ready(() => {
 
   function onFunctionChange() {
     try {
-      $('#screen-code').val('');
+      $("#screen-code").val("");
       resetGrid();
       updateGrid();
       drawGrid();
-      $('#screen-code').val(gridToPairOfUint160());
+      $("#screen-code").val(gridToPairOfUint160());
     } catch (e) {
-      console.log('Invalid function:', e);
+      console.log("Invalid function:", e);
     }
   }
 
   function onScreenCodeChange() {
-    gridFromPairOfUint160($('#screen-code').val());
+    gridFromPairOfUint160($("#screen-code").val());
   }
 
   resetGrid();
   // onFunctionChange();
-  $('#x-min').on('input', onFunctionChange);
-  $('#x-max').on('input', onFunctionChange);
-  $('#y-min').on('input', onFunctionChange);
-  $('#y-max').on('input', onFunctionChange);
-  $('#user-function').on('input', onFunctionChange);
-  $('#screen-code').on('input', onScreenCodeChange);
+  $("#x-min").on("input", onFunctionChange);
+  $("#x-max").on("input", onFunctionChange);
+  $("#y-min").on("input", onFunctionChange);
+  $("#y-max").on("input", onFunctionChange);
+  $("#user-function").on("input", onFunctionChange);
+  $("#screen-code").on("input", onScreenCodeChange);
 });
